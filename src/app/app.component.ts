cd7b4ced9d034c5e3c750cae3d2bbb4e4cb61225
ggw <ggw>
2021-09-17 17:28:20 +0900

    0917

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,46 +15,8 @@ import "firebase/storage"
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  back_clicked = 0;
-
-  constructor(
-    private toastCtrl: ToastController,
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {this.initializeApp()}
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-        this.statusBar.styleDefault();
-        this.splashScreen.hide();
-    });
-}
-  
-  ngOnInit() {
-    this.appExitConfig()
-  }
-
-  private appExitConfig() {
-    this.platform.backButton.subscribe(async () => {
-        if (this.back_clicked === 0) {
-            this.back_clicked++;
-
-            const toast = await this.toastCtrl.create({
-                message: '뒤로가기 버튼을 한번 더 누르시면 앱이 종료됩니다.',
-                duration: 2000
-            });
-            toast.present();
-
-            setTimeout(() => {
-                this.back_clicked = 0;
-            }, 2000);
-        } else {
-            navigator['app'].exitApp();
-        }
-    });
-}
+export class AppComponent {
+  constructor() {}
 }
 
 const firebaseConfig = {
