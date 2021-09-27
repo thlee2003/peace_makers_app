fafd9d48e2ba2dfd34eb93362aa0dd71c8eef4e1
ggw <ggw>
2021-09-27 18:00:35 +0900

    Change_Mainpage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,36 +7,34 @@ import firebase from 'firebase';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-
+export class HomePage implements OnInit {
   selectTab: any;
   @ViewChild('tabs') homes: IonTabs;
 
-  page = "my-page"
+  page = 'my-page';
 
-  constructor(private router: Router,) {}
+  constructor(private router: Router) {}
 
   setCurrentTab(event) {
     this.selectTab = this.homes.getSelected();
-    console.log(this.router.url)
+    console.log(this.router.url);
   }
 
   clickTab1() {
-    this.router.navigateByUrl('/home/support')
+    this.router.navigateByUrl('/home/support');
   }
 
   clickTab2() {
-    this.router.navigateByUrl('/home/'+this.page)
+    this.router.navigateByUrl('/home/' + this.page);
   }
 
   async ngOnInit() {
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user && user.emailVerified) {
-          this.page = "my-page-login"
-        } else {
-          this.page = "my-page"
-        }   
-      });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user && user.emailVerified) {
+        this.page = 'my-page-login';
+      } else {
+        this.page = 'my-page';
+      }
+    });
   }
-
 }
